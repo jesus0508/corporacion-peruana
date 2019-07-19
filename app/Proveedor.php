d6969a4144837f0debef15d658cd11d8007b52ca
navichicken <45251599+navichicken@users.noreply.github.com>
2019-07-19 18:29:19 -0500

    proveedor-modelo

--- app/Proveedor.php
+++ app/Proveedor.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Proveedor extends Model
 {
     protected $table = 'proveedores';
+    protected $fillable= ['planta','razon_social','representante','celular'];
 }
