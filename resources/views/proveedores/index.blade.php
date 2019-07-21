8c543d83fcf608bb0cfdd29359e4579a22724eda
navichicken <45251599+navichicken@users.noreply.github.com>
2019-07-21 09:15:50 -0500

    proveedor index

--- resources/views/proveedores/index.blade.php
+++ resources/views/proveedores/index.blade.php
@@ -20,22 +20,44 @@
         </div>
         <!-- /.box-header -->
         <div class="box-body">
-          <table id="example1" class="table table-bordered table-striped">
+          <table id="proveedores" class="table table-bordered table-striped">
             <thead>
               <tr>
-                <th>Planta</th>
                 <th>Razon Social</th>
+                <th>Dirección</th>
                 <th>Representante</th>
                 <th>Celular</th>
+                <th>Acciones</th>
               </tr>
             </thead>
             <tbody>
               @foreach ($proveedores as $proveedor)
                 <tr>
-                  <td>{{$proveedor->planta}}</td>
+                 
                   <td>{{$proveedor->razon_social}}</td>
+                  <td>{{$proveedor->direccion}}</td>
                   <td>{{$proveedor->representante}}</td>
                   <td>{{$proveedor->celular}}</td>
+                  <td>  
+
+                    <button class="btn btn-warning" data-toggle="modal" data-target="#modal-edit-proveedor"
+                              data-id="{{$proveedor->id}}" data-representante="{{$proveedor->representante}}" data-celular="{{$proveedor->celular}}"
+                              data-razon_social="{{$proveedor->razon_social}}" data-direccion="{{$proveedor->direccion}}">
+                      <span class="glyphicon glyphicon-edit"></span>
+                    </button>
+
+
+                     <form style="display:inline" method="POST" action="{{ route('proveedores.destroy', $proveedor->id) }}">
+                            @csrf
+                             @method('DELETE')
+                              <button class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span></button>
+                      </form>
+                    
+
+
+                  </td>
+
+
                 </tr>
               @endforeach
             </tbody>
@@ -44,5 +66,8 @@
       </div> <!-- end box -->
     </div>
   </div><!-- end row -->
+    @include('proveedores.edit')
+    @include('partials.session-status') 
 </section>
+
 @endsection
