33393c4939bed0751f95f25f14bf6ca814c9f266
navichicken <45251599+navichicken@users.noreply.github.com>
2019-07-27 18:04:26 -0500

    bug al crear un nuevo proveedor arreglado

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -1,4 +1,4 @@
-<form action="{{route('clientes.store')}}" method="post">
+<form action="{{route('proveedores.store')}}" method="post">
   @csrf
   <div class="row">
     <!-- left column -->
@@ -6,83 +6,65 @@
       <!-- general form elements -->
       <div class="box box-success">
         <div class="box-header with-border">
-          <h3 class="box-title">Datos principales</h3>
+          <h3 class="box-title">Datos principales*</h3>
         </div><!-- /.box-header -->
-        <div class="box-body">
-          <div class="form-group @error('ruc') has-error @enderror">
-            <label for="ruc">RUC</label>
-            <input id="ruc" type="text" class="form-control" 
-                    name="ruc" placeholder="Ingrese su RUC">
-            @error('ruc')
+        {{-- <form role="form"> --}}
+          <div class="box-body">
+               <div class="form-group @error('razon_social') has-error @enderror">
+              <label for="razon_social">Razon Social*</label>
+              <input id="razon_social" type="text" class="form-control" name="razon_social" placeholder="Ingrese la Razon Social" required>
+              @error('razon_social')
             <span class="help-block" role="alert">
               <strong>{{ $message }}</strong>
             </span>
             @enderror
-          </div>
-          <div class="form-group @error('razon_social') has-error @enderror">
-            <label for="razon_social">Razón Social</label>
-            <input id="razon_social" type="text" class="form-control" 
-                    name="razon_social" placeholder="Ingrese la Razon Social">
-            @error('razon_social')
-            <span class="help-block" role="alert">
-              <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-          </div>
-          <div class="form-group @error('telefono') has-error @enderror">
-            <label for="telefono">Teléfono</label>
-            <input id="telefono" type="tel" class="form-control"
-                    name="telefono" placeholder="Ingrese el numero de telefono">
-            @error('telefono')
-            <span class="help-block" role="alert">
-              <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-          </div>
-        </div><!-- /.box-body -->
+            </div>
+            <div class="form-group">
+                <label for="ruc">RUC*</label>
+               <input id="ruc" type="text" class="form-control" name="ruc" placeholder="Ingrese  el ruc del proveedor">           
+            </div>
+
+          
+            
+         
+         
+          </div><!-- /.box-body -->
+        {{-- </form> --}}
       </div><!-- /.box -->
     </div>
     <!--/.col (left) -->
-  
+ 
     <div class="col-md-6">
       <!-- general form elements -->
       <div class="box box-success">
         <div class="box-header with-border">
           <h3 class="box-title">Datos secundarios</h3>
         </div><!-- /.box-header -->
-        <div class="box-body">
-          <div class="form-group @error('tipo') has-error @enderror">
-            <label for="tipo">Tipo</label>
-            <select id="tipo" class="form-control" name="tipo">
-              <option value="1">Grifo</option>
-              <option value="2">Normal</option>
-            </select>
-            @error('tipo')
-            <span class="help-block" role="alert">
-              <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-          </div>
-          <div class="form-group @error('dirrecion') has-error @enderror">
-            <label for="direccion">Dirección</label>
-            <input id="direccion" type="text" class="form-control" 
-                    name="direccion" placeholder="Ingrese la direccion">
-            @error('direccion')
-            <span class="help-block" role="alert">
-              <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-          </div>
-        </div><!-- /.box-body -->
+        <!-- form start -->
+        {{-- <form role="form"> --}}
+          <div class="box-body">
+            <div class="form-group">
+                <label for="representante">Representante</label>
+               <input id="representante" type="text" class="form-control" name="representante" placeholder="Ingrese  representate">
+           
+            </div>
+         
+        
+
+
+            
+
+          </div><!-- /.box-body -->
+        {{-- </form> --}}
       </div><!-- /.box -->
     </div>
     <!--/.col (right) -->
   </div> <!-- /.row-top -->
   <div class="row">
     <div class="col-md-12">
-      <button type="submit" class="btn btn-lg btn-primary">
+      <button type="submit" class="btn btn-lg btn-success">
         <i class="fa fa-plus-square-o"> </i>
-        Registrar nuevo cliente
+        Registrar nuevo proveedor
       </button>
     </div>
   </div> <!-- /.row-bottom -->
