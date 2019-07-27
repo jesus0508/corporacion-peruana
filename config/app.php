4a879bc3f6b9051cf8d111f96367ba1439f95675
navichicken <45251599+navichicken@users.noreply.github.com>
2019-07-27 18:19:43 -0500

    agregando provider y alias del datatable

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Yajra\Datatables\DatatablesServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -174,6 +174,7 @@ return [
         // CorporacionPeru\Providers\BroadcastServiceProvider::class,
         CorporacionPeru\Providers\EventServiceProvider::class,
         CorporacionPeru\Providers\RouteServiceProvider::class,
+
 
     ],
 
@@ -225,6 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Datatables' => Yajra\Datatables\Facades\Datatables::class,
     ],
 
 ];
