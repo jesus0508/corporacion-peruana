17ba3ecb1d8ab477aaeb5615e30b7cb5d99cd72c
navichicken <45251599+navichicken@users.noreply.github.com>
2019-07-29 04:38:14 -0500

    added controllers vehiculo,tranportista, factProve

---
+++ app/Http/Controllers/FacturaProveedorController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace CorporacionPeru\Http\Controllers;
+
+use Illuminate\Http\Request;
+use CorporacionPeru\Pedido;
+
+class FacturaProveedorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $pedidos = Pedido::all();
+        return view( 'facturas.create',compact(  'pedidos' ) );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+      public function show($id)
+    {
+        
+         $pedido=Pedido::findOrFail($id);
+         return $pedido;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
